62a15dc232ac1ef7eab4e0f0030a88baae247430
aarontbarratt <aarontbarratt@gmail.com>
2023-08-28 23:44:03 +0100

    improved character table and protected against SQL injection

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from db import cursor
 app = FastAPI()
 
 
+def sanitize(string: str) -> str:
+    """
+    strips and lowers strings
+    :param string: any string
+    :return: sanitized string
+    """
+    string = string.strip().lower()
+    return string
+
+
 @app.get("/")
 def read_root():
     return {"Hello", "World"}
@@ -12,21 +22,35 @@ def read_root():
 
 @app.get("/character")
 def get_characters():
-    cursor.execute(f"SELECT character_name FROM characters")
+    """gets data for all characters
+    :return: id, full_name, short_name, nationality, sex, age, weight_kg, height_cm
+    """
+    query: str = f"""
+        SELECT id, full_name, short_name, nationality, sex, age, weight_kg, height_cm
+        FROM character
+        order by id;
+    """
+    cursor.execute(query)
     result = cursor.fetchall()
     return result
 
 
 @app.get("/character/{name}")
 def get_character(name: str):
-    """ Gets character data from the database
-    :param name: Character name as a string
-    :return: Character data
+    """gets the provided characters data from the database
+    :param name: characters first name as a string
+    :return: id, full_name, short_name, nationality, sex, age, weight_kg, height_cm
     """
-    cursor.execute(f"SELECT character_name FROM characters WHERE character_name='{name}'")
-    result = cursor.fetchone()[0]
+    name = sanitize(name)
+    query = """
+        SELECT id, full_name, short_name, nationality, sex, age, weight_kg, height_cm
+        FROM character 
+        WHERE short_name=%s
+        """
+    cursor.execute(query, (name,))  # (name,) creates a tuple with a single value
+    result = cursor.fetchone()
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     get_character()
