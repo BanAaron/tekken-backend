187062f0d07ab1e87649b9b9d41fa2dd3785ec3c
Khephren <aarontbarratt@gmail.com>
2023-11-19 20:41:09 +0000

    cors fix

--- handlers/cors.go
+++ handlers/cors.go
@@ -2,10 +2,7 @@ package handlers
 
 import "net/http"
 
-// frontendHost should be the IP Address and port of the front end app
-var frontendHost = "http://localhost:5173"
-
 // EnableCors enables cors from the frontend host
 func EnableCors(writer *http.ResponseWriter) {
-	(*writer).Header().Set("Access-Control-Allow-Origin", frontendHost)
+	(*writer).Header().Set("Access-Control-Allow-Origin", "*")
 }
