db2f52891879b9dfb27995f35f9163b2ddde7fbc
Khephren <aarontbarratt@gmail.com>
2023-11-19 12:51:28 +0000

    updates for railroad

--- database/database.go
+++ database/database.go
@@ -3,7 +3,6 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
 	"strconv"
@@ -16,10 +15,10 @@ var Db *sql.DB
 const driver = "postgres"
 
 func getEnvironmentVariables() (username string, password string, host string, dbName string, port int, err error) {
-	err = godotenv.Load(".env")
-	if err != nil {
-		return
-	}
+	//err = godotenv.Load(".env")
+	//if err != nil {
+	//	return
+	//}
 	username = os.Getenv("DB_USERNAME")
 	password = os.Getenv("DB_PASSWORD")
 	host = os.Getenv("DB_HOST")
